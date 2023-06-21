35b9c639ec58308011c79468a6de166e35adb160
Sebastian W <117441091+Sebastiandotpy@users.noreply.github.com>
2023-06-21 14:51:02 +0200

    added solution

--- src/app.py
+++ src/app.py
@@ -1,14 +1,12 @@
-import math
-
 from check import print_rectangle_properties
-
-class Rectangle:
-    pass
-    # TODO: Add code here
+from rectangle import Rectangle
 
 def main():
-    pass
-    # TODO: Add code here
+    rectangle1 = Rectangle(9.0, 12.0)
+    rectangle2 = Rectangle(17.0, 13.0)
+
+    print_rectangle_properties(rectangle1)
+    print_rectangle_properties(rectangle2)
 
 if __name__ == "__main__":
     main()
